e84e7b6c5fa63f6de9a6bb5cc54ec0630e9bdbe9
Ubuntu <ubuntu@ip-172-31-26-95.ec2.internal>
2022-04-23 14:00:56 +0000

    Deploy on ec2nd rds

--- myblog/app/__init__.py
+++ myblog/app/__init__.py
@@ -13,4 +13,6 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 bootstrap = Bootstrap(app)
+
 from app import routes, models,errors
+
--- myblog/config.py
+++ myblog/config.py
@@ -3,7 +3,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('key') or 'ductran123'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-    'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI =  "mysql+pymysql://admin:tranduc123@habitmakr.cq4hhb9aoctr.us-east-1.rds.amazonaws.com:3306/innodb" #os.environ.get('DATABASE_URL') or \'sqlite:///' + os.path.join(basedir, 'myblog.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     POSTS = 3
