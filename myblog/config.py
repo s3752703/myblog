1742cb2fa91a7d9c146427cd3948b1847644cca8
Ubuntu <ubuntu@ip-172-31-27-67.ec2.internal>
2022-04-25 11:30:39 +0000

    Added gunicorn

--- myblog/config.py
+++ myblog/config.py
@@ -6,9 +6,9 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = "mysql+pymysql://admin:tranduc123@habitmakr.cq4hhb9aoctr.us-east-1.rds.amazonaws.com:3306/innodb"
     UPLOADED_PHOTOS_DEST = 'uploads/photos'
     S3_BUCKET = 'mybloghost'
-    S3_KEY = "ASIAR6R4OLQMJ5DDNO4Y"
+    S3_KEY = "DgZTg0tpvhD1fPQD6haIuOs0Ptcrm6YAo7SuuVZA"
     S3_SECRET = '4jWTuPUnJVgBT/qtuJZedgv2OyRPUIAN/u8L00C5'
-    S3_SESSION_TOKEN = "FwoGZXIvYXdzENv//////////wEaDPcpDayX1VwlYUtRbyLNAQB9uLNKnagCuURF2qW55yntUc49993QlWDUlrLyxsSwRo9ipxuD69QTX15j+yHLUJGsjn4hvjksPxY7+qC95IsXQqXSy2B/woYuIUXW9f3k+ed3n9q7wK8dY6oGZA1HkouhRYJU/AN2JjOfPimT6JN7ZzMVkpj5JSjZCX7CJ0jkcfYuSRqIdKeZZavf6QR3+FfSGJYCrQMpweIaOCWaza515UkeYR7l4ingF42JHjl+z0gzBYS40S9DWCU1lnubPUYWXMGMbYBUIoubZtQo8vqXkwYyLX9J3a5+En3s+FwX0Sqv0FsWAweSqXjd1VZNWi7Nwm5pb30pXHqYp/v12XXl9w=="
+    S3_SESSION_TOKEN = "FwoGZXIvYXdzEOT//////////wEaDKbFUSNLJGBvN04UZCLNAa/wECUOBXX4dcLiMbZURriuOwLgniaHGyjwxk/0o4I5GrOrwz4uE7lDGd4K0P085uI6yntnhjYN7SnNusxE+U+5qsYxXk1RrhIVbmAdR1Wrrbfj0dpqCiznjunCsIOR1mk0jfrsrf9J/2rzUz4pcubJ+mGnJaaE2H61sovXWBBq8DKRNCikH5kiyTdlP3mRhB9rmMVSA3Htezxzkyuy0wPns0eFVbMj36YPhGVC/KzfWLjl3gdqXVHhE1jH/cW8X7elvL13W3/ixuzFl2co3vmZkwYyLUDqdduqeWbsQfmpgAvZGYQdMtEJc5rjw5m7oBQmXHADWKCDZvv310OYBNC+dQ=="
     S3_LOCATION = 'http://{}.s3.amazonaws.com/'.format(S3_BUCKET)
     UPLOAD_EXTENSIONS = ['.jpg', '.png', '.gif','.jpeg','.raw']
     #"mysql+pymysql://admin:tranduc123@habitmakr.cq4hhb9aoctr.us-east-1.rds.amazonaws.com:3306/innodb" #os.environ.get('DATABASE_URL') or \'sqlite:///' + os.path.join(basedir, 'myblog.db')
